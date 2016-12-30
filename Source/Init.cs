7fcd602af79a10854e8783695e60f40dbacaf8de
apkd <apkd@users.noreply.github.com>
2016-12-30 08:23:11 +0100

    Fixed duplicate mod listings

--- Source/Init.cs
+++ Source/Init.cs
@@ -12,10 +12,12 @@ namespace ModlistMod
 			Log.Message($"Saving the list of installed mods to modlist.txt");
 
 			var modlist = new System.Text.StringBuilder();
+
 			var mods = ModLister.AllInstalledMods
 				.Where(m => m.Active)
 				.Where(m => !m.IsCoreMod)
-				.Where(m => m.Name != "Modlist");
+				.Where(m => m.Name != "Modlist")
+				.Distinct();
 
 			foreach (var mod in mods)
 				modlist.AppendLine(mod.Name);
