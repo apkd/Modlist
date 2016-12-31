7b34bfe1431a5812c04f151d90d7d62bc99c68d4
apkd <apkd@users.noreply.github.com>
2016-12-31 04:20:46 +0100

    Fixed duplicate mod listings (again). Added download links.

--- Source/Init.cs
+++ Source/Init.cs
@@ -5,25 +5,48 @@ using Verse;
 
 namespace ModlistMod
 {
+	/// <summary> This code is ran once while the mod is being loaded. </summary>
 	public class Init : Def
 	{
-		public Init()
+		const string steam_workshop_uri = "http://steamcommunity.com/sharedfiles/filedetails/?id=";
+
+		static void SaveModlist(string filename)
 		{
-			Log.Message($"Saving the list of installed mods to modlist.txt");
+			Log.Message($"Saving the list of installed mods to {filename}");
 
 			var modlist = new System.Text.StringBuilder();
 
 			var mods = ModLister.AllInstalledMods
-				.Where(m => m.Active)
-				.Where(m => !m.IsCoreMod)
-				.Where(m => m.Name != "Modlist")
-				.Distinct();
+				.Where(m => m.Active) // only active mods
+				.Where(m => !m.IsCoreMod) // ignore the Core mod
+				.Where(m => m.Name != "Modlist") // ignore self
+				.GroupBy(x => x.Name).Select(m => m.First()) // select distinct by name
+				.OrderBy(x => x.Name); // sort
 
 			foreach (var mod in mods)
-				modlist.AppendLine(mod.Name);
+			{
+				string link = mod.OnSteamWorkshop ?
+					steam_workshop_uri + mod.GetPublishedFileId().m_PublishedFileId : // use steam uri
+					mod.Url; // use mod's own download link as a fallback
+
+				modlist.AppendLine($"{mod.Name} | {link}");
+			}
 
 			var path = Path.Combine(Environment.CurrentDirectory, "modlist.txt");
+			if (File.Exists(path)) File.Delete(path);
 			File.WriteAllText(path, modlist.ToString());
+		}
+
+		static Init()
+		{
+			try
+			{
+				SaveModlist("modlist.txt");
+			}
+			catch (Exception)
+			{
+				Log.Warning("Couldn't save the modlist.");
+			}
 		}
 	}
 }
